70f42ec06bd66a146ac911e79aa712cedbd5dc1a
kartikeya <64151789+ksrepo9@users.noreply.github.com>
2023-08-03 21:29:03 +0530

    Update stack.sh

--- stack.sh
+++ stack.sh
@@ -3,19 +3,3 @@ TOM_URL=https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.75/bin/apache-tomcat-9.0.7
 TOM_HOME_PATH=/opt/apache-tomcat-9.0.75.tar.gz
 TOM_WEBAPP=/opt/apache-tomcat-9.0.75/webapps
 
-echo " Downloading java"
-wget https://files01.tchspt.com/temp/jdk-9.0.4_linux-x64_bin.rpm
-
-echo " Installing JAVA "
-rpm -ivh jdk-9.0.4_linux-x64_bin.rpm
-
-echo "Downloading Tomcat "
-wget $TOM_URL -O $TOM_HOME_PATH
-
-echo " Extracting Tomcat "
-cd /opt/
-tar -xvf $TOM_HOME_PATH 
-
-echo " clear webapps dir"
-cd $TOM_WEBAPP
-rm -rf *;
